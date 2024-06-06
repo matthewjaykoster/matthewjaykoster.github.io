95f0f23927f8001a24c7e9ecc8c5988b2a9a82d2
Matt Koster <mkoster@ostusa.com>
2024-06-06 12:13:41 -0400

    [Lab: Center Meeting] Updated React className convention

--- src/center-meeting/App.js
+++ src/center-meeting/App.js
@@ -25,19 +25,19 @@ function LocationOptions( location ) {
 
     return (
         <>
-            <div class="list-group-item py-3 lh-tight">
-                <div class="d-flex w-100 align-items-center">
-                    <strong class="col-8 mb-1 text-truncate" >{location.name}</strong>
-                    <div class="offset-1 col-3 d-flex justify-content-end">
-                        <button type="button" class="btn btn-default">
-                            <small><i class="fa fa-pen"></i></small>
+            <div className="list-group-item py-3 lh-tight">
+                <div className="d-flex w-100 align-items-center">
+                    <strong className="col-8 mb-1 text-truncate" >{location.name}</strong>
+                    <div className="offset-1 col-3 d-flex justify-content-end">
+                        <button type="button" className="btn btn-default">
+                            <small><i className="fa fa-pen"></i></small>
                         </button>
-                        <button type="button" class="btn btn-default">
-                            <small><i class="fa fa-trash"></i></small>
+                        <button type="button" className="btn btn-default">
+                            <small><i className="fa fa-trash"></i></small>
                         </button>
                     </div>
                 </div>
-                <div class="col-12 mb-1 small">
+                <div className="col-12 mb-1 small">
                     {locationEditor}
                 </div>
             </div>
@@ -66,27 +66,27 @@ export default function CenterMeetingApp() {
     ) );
     return (
         <>
-            <div class="col-3 p-0">
-                <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-                    <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-                        <span class="fs-5 fw-semibold">Locations</span>
+            <div className="col-3 p-0">
+                <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+                    <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+                        <span className="fs-5 fw-semibold">Locations</span>
                     </div>
-                    <div id="location-list" class="list-group list-group-flush border-bottom scrollarea">
+                    <div id="location-list" className="list-group list-group-flush border-bottom scrollarea">
                         {locationItems}
-                        <div class="list-group-item list-group-item-action py-3 lh-tight mouseover-action">
-                            <div class="align-items-center">
-                                <i class="fa fa-circle-plus"></i>
+                        <div className="list-group-item list-group-item-action py-3 lh-tight mouseover-action">
+                            <div className="align-items-center">
+                                <i className="fa fa-circle-plus"></i>
                                 Add Location
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-1 p-0">
-                <div class="b-divider"></div>
+            <div className="col-1 p-0">
+                <div className="b-divider"></div>
             </div>
-            <div class="col">
-                <img class="img-fluid" src="/dist/assets/img/under-construction.png" alt="..." />
+            <div className="col">
+                <img className="img-fluid" src="/dist/assets/img/under-construction.png" alt="..." />
             </div>
         </>
     );
